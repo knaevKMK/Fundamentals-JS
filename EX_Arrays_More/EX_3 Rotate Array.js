1d203be3cb4be9b75f065dff5fc410d9a03bbecf
Krasimir Kanev <knev@mail.bg>
2021-01-06 22:50:21 +0200

    Create EX_3 Rotate Array.js

---
+++ EX_Arrays_More/EX_3 Rotate Array.js
@@ -0,0 +1,20 @@
+function solve(temp) {
+    let indexReverce= (temp.pop());
+   if (indexReverce.length>2) {
+        console.log('Empty');
+        return;
+    }
+
+    while (indexReverce >= temp.length) {
+        indexReverce -= temp.length;
+    }
+    indexReverce = temp.length - indexReverce;
+    let end = temp.slice(0, indexReverce);
+    temp = temp.slice(indexReverce, temp.length).concat(end);
+    console.log(temp.toString().replace(/,/g, ' '));
+
+}
+
+solve(['1', '2', '3', '4', '2']);
+solve(['Banana', 'Orange', 'Coconut', 'Apple', '15']);
+solve(['remove', 'remove', 'remove']);
