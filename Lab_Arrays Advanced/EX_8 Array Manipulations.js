b304b6aa49461bf86813019707a0754f359aa155
Krasimir Kanev <knev@mail.bg>
2021-01-08 01:07:42 +0200

    Create EX_8 Array Manipulations.js

---
+++ Lab_Arrays Advanced/EX_8 Array Manipulations.js
@@ -0,0 +1,28 @@
+function solve(input) {
+    let array = input[0].split(' ');
+
+    for (let index = 1; index < input.length; index++) {
+        let token = input[index].split(' ');
+        switch (token[0]) {
+            case 'Add':
+                array.push(token[1]);
+                break;
+            case 'Remove':
+                array=array.filter(el=>el!==token[1]);
+                break;
+            case 'RemoveAt':
+                array.splice(token[1], 1);
+                break;
+            case 'Insert':
+                array.splice(token[2], 0, token[1]);
+                break;
+        }
+    }
+    console.log(array.join(' '));
+}
+solve(['4 19 2 53 6 43',
+    'Add 3',
+    'Remove 2',
+    'RemoveAt 1',
+    'Insert 8 3']
+);
