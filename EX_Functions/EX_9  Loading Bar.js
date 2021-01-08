5cdbf0438f95398e4dd173f9c0c49319d9686e49
Krasimir Kanev <knev@mail.bg>
2021-01-09 00:29:53 +0200

    Create EX_9  Loading Bar.js

---
+++ EX_Functions/EX_9  Loading Bar.js
@@ -0,0 +1,17 @@
+function main(num) {
+    let count = num / 10;
+
+    if (count == 10) {
+        console.log(`100% Complete!`);
+        console.log(`[%%%%%%%%%]`);
+    } else {
+        let pr = '%';
+        let dot = '.';
+        console.log(`${num}%[${pr.repeat(count)}${dot.repeat(10 - count)}]`);
+        console.log(`Still loading...`);
+    }
+
+}
+main(30);
+main(50);
+main(100);
